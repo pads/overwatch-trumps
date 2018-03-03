fd5041c9ece8d056f097d3c55cd57347b2e58c5c
Ben Paddock <pads@thisispads.me.uk>
2018-03-03 20:39:47 +0000

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatGridListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
