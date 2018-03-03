6b274f4eb476de90f6e43c1be6c9155b62482cbf
Ben Paddock <pads@thisispads.me.uk>
2018-03-03 20:58:46 +0000

    Add service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { environment } from '@environments/environment';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatGridListModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    environment.production
+      ? ServiceWorkerModule.register('/ngsw-worker.js')
+      : [],
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
